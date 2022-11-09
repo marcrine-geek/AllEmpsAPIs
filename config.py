e41ac21527183eeb53b74e7d5300d29c29981aec
marcrine <marcrinemm@gmail.com>
2022-11-09 08:22:28 +0300

    config file

---
+++ config.py
@@ -0,0 +1,42 @@
+import os
+
+postgres_local_base = 'postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}'.format(
+        user=os.environ.get('FLASK_DATABASE_USER', 'allemps'),
+        password=os.environ.get('FLASK_DATABASE_PASSWORD', 'allemps'),
+        host=os.environ.get('FLASK_DATABASE_HOST', '127.0.0.1'),
+        port=os.environ.get('FLASK_DATABASE_PORT', 5432),
+        db_name=os.environ.get('FLASK_DATABASE_NAME', 'allemps'),
+    )
+# postgres_local_base = os.environ['DATABASE_URL']
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'nothing')
+    DEBUG = False
+    TOKEN_EXPIRE_HOURS = (24 * 365)
+
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = postgres_local_base
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class TestingConfig(Config):
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = postgres_local_base
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = postgres_local_base
+
+
+config_by_name = dict(
+    dev=DevelopmentConfig,
+    test=TestingConfig,
+    prod=ProductionConfig
+)
+
+key = Config.SECRET_KEY
