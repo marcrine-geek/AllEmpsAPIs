c56cd7459e4891f4c2d8142823750fd5ee4d5bb7
marcrine <marcrinemm@gmail.com>
2022-12-01 10:39:20 +0300

    updates

--- backend/models/channelmembers.py
+++ backend/models/channelmembers.py
@@ -8,6 +8,5 @@ class CmembersModel(BaseClass, db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    user_firstname = db.Column(db.String(255), db.ForeignKey('users.firstname'), nullable=False)
-    user_lastname = db.Column(db.String(255), db.ForeignKey('users.lastname'), nullable=False)
+    
     
